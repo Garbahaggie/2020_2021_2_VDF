293176f4267892fcf2cad4fe0734763bd3243bc1
Garbahaggie <bbaranyi55@gmail.com>
2021-05-11 12:57:34 +0200

    12. óra befejezve

--- Kolcsonzo_2021/AppHtml/DataFlexReports/Previewer.js
+++ Kolcsonzo_2021/AppHtml/DataFlexReports/Previewer.js
@@ -2,7 +2,7 @@ if(!DR){
     var DR = { };
 }
 
-DR.sVersion = "7.0.0.191";
+DR.sVersion = "8.0.3.200";
 
 DFERR_DATAFLEX_REPORTS = 4425;
 
@@ -165,10 +165,6 @@ isThemeCSSLoaded : function(sCurrentPath){
         sLocalTheme = this._sTheme;
         if(sLocalTheme){
             sLocalTheme = sLocalTheme.toLowerCase();
-        }
-        // WebApp framework should do this! Without this code the switching themes does not work correctly with regards to the tool-bar
-        if(document.body){
-            df.dom.addClass(document.body, this._sTheme);
         }
         aStyles = df.dom.query(document, "link", true);
         for(i = 0; i < aStyles.length && bThemeLoaded === false; i++){
@@ -440,15 +436,15 @@ showReport : function(){
     var i, j, aReportData, aPageData, reportHtml = "";
 
     if(this.peMode === 0){
-		aReportData = this._tActionData.c;
+        aReportData = this._tActionData.c;
         if(aReportData.length > 0){
-		    for(i = 0; i < aReportData.length; i++){
-			    aPageData = this.deserializeVT(aReportData[i]);
-			    for(j = 0; j < aPageData.length; j++){
-				    reportHtml += Base64.decode(aPageData[j]);
-		        }
-	        }
-			this._sReportHtml = reportHtml;
+            for(i = 0; i < aReportData.length; i++){
+                aPageData = this.deserializeVT(aReportData[i]);
+                for(j = 0; j < aPageData.length; j++){
+                    reportHtml += aPageData[j];
+                }
+            }
+            this._sReportHtml = reportHtml;
             this.refreshPreview();
             this.piPage = this.get_piPage();
             this.fire('OnOpenReport');
@@ -458,8 +454,8 @@ showReport : function(){
             throw new df.Error(DFERR_DATAFLEX_REPORTS, "No HTML data received.");
         }
     }else{
-		aReportData = this.deserializeVT(this._tActionData);
-		if(aReportData.length > 0){
+        aReportData = this.deserializeVT(this._tActionData);
+        if(aReportData.length > 0){
             for(i = 0; i < aReportData.length; i++){
                 reportHtml += "<div class='page' style='position: relative;'>";
                 reportHtml += "<img src='"+aReportData[i]+"' width='100%' height='100%'>";
@@ -467,13 +463,13 @@ showReport : function(){
             }
             this._sReportHtml = reportHtml;
             this.refreshPreview();
-			this.piPage = this.get_piPage();
-			this.fire('OnOpenReport');
-			
+            this.piPage = this.get_piPage();
+            this.fire('OnOpenReport');
+
             this.updateToolbar();
         }else{
             throw new df.Error(DFERR_DATAFLEX_REPORTS, "No image data received.");
-		}
+        }
     }
 },
 
@@ -526,7 +522,7 @@ doPrint : function(){
 
 set_piZoom : function(iVal){
     var sTransform, nScale, eElem = this._eViewSizer;
-	
+
     if(this._eViewSizer && this._eViewPort){
         nScale = iVal / 100;
         sTransform = "scale(" + nScale + ", " + nScale + ")";
@@ -544,7 +540,7 @@ set_piZoom : function(iVal){
         }
         this.piZoom = iVal;
         this.recalcSize();
-	}
+    }
 },
 
 set_piPage : function(iPage){
@@ -675,130 +671,3 @@ set_pbShowToolbar: function(bVal){
 }
 }
 );
-
-var Base64 = {
-// private property
-_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-// public method for encoding
-encode : function (input) {
-    var output = "";
-    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
-
-    input = Base64._utf8_encode(input);
-
-    while (i < input.length) {
-
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
-        }
-
-        output = output +
-        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-
-    }
-
-    return output;
-},
-
-// public method for decoding
-decode : function (input) {
-    var output = "";
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-    while (i < input.length) {
-
-        enc1 = this._keyStr.indexOf(input.charAt(i++));
-        enc2 = this._keyStr.indexOf(input.charAt(i++));
-        enc3 = this._keyStr.indexOf(input.charAt(i++));
-        enc4 = this._keyStr.indexOf(input.charAt(i++));
-
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
-
-        output = output + String.fromCharCode(chr1);
-
-        if (enc3 != 64) {
-            output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 != 64) {
-            output = output + String.fromCharCode(chr3);
-        }
-
-    }
-
-    output = Base64._utf8_decode(output);
-
-    return output;
-},
-
-// private method for UTF-8 encoding
-_utf8_encode : function (string) {
-    string = string.replace(/\r\n/g,"\n");
-    var utftext = "";
-
-    for (var n = 0; n < string.length; n++) {
-        var c = string.charCodeAt(n);
-        if (c < 128) {
-            utftext += String.fromCharCode(c);
-        }
-        else if((c > 127) && (c < 2048)) {
-            utftext += String.fromCharCode((c >> 6) | 192);
-            utftext += String.fromCharCode((c & 63) | 128);
-        }
-        else {
-            utftext += String.fromCharCode((c >> 12) | 224);
-            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-            utftext += String.fromCharCode((c & 63) | 128);
-        }
-    }
-
-    return utftext;
-},
-
-// private method for UTF-8 decoding
-_utf8_decode : function (utftext) {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
-
-    while ( i < utftext.length ) {
-        c = utftext.charCodeAt(i);
-        if (c < 128) {
-            string += String.fromCharCode(c);
-            i++;
-        }
-        else if((c > 191) && (c < 224)) {
-            c2 = utftext.charCodeAt(i+1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-            i += 2;
-        }
-        else {
-            c2 = utftext.charCodeAt(i+1);
-            c3 = utftext.charCodeAt(i+2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-            i += 3;
-        }
-    }
-
-    return string;
-}
-}
